70b2fb069ee61d182f0d03813bf4514658c85c17
seresusly <carlos.resurreccion@obf.ateneo.edu>
2023-01-23 16:30:18 +0800

    ch2.3

---
+++ R4DS/penguins.R
@@ -0,0 +1,2 @@
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) + 
+  geom_point()
--- penguins.R
+++
@@ -1,6 +0,0 @@
-ggplot(
-  data = penguins,
-  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = island)
-) +
-  geom_point() +
-  geom_smooth(se = FALSE)

5710afb46d26b81334d3531004851dab532fc961
seresusly <carlos.resurreccion@obf.ateneo.edu>
2023-01-23 14:59:08 +0800

    ch2

--- penguins.R
+++ penguins.R
@@ -1,14 +1,6 @@
 ggplot(
   data = penguins,
-  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = island)
 ) +
-  geom_point(aes(color = species, shape = species), na.rm = FALSE) +
-  geom_smooth() +
-  labs(
-    title = "Body mass and flipper length",
-    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
-    x = "Flipper length (mm)",
-    y = "Body mass (g)",
-    color = "Species",
-    shape = "Species"
-  )
+  geom_point() +
+  geom_smooth(se = FALSE)

ab709e621e427f6331473999923a772aee3f3b57
seresusly <carlos.resurreccion@obf.ateneo.edu>
2023-01-26 08:40:53 +0000

    ch visualize

---
+++ R4DS/Visualize.R
@@ -0,0 +1,5 @@
+#ggplot(mpg, aes(x = hwy, y = displ)) + 
+  #geom_point(shape = 24, color = "black", fill = "pink")
+#library(tidyverse)
+#ggplot(mpg, aes(x = displ, y = hwy, color = displ < 5)) + 
+  #geom_point()

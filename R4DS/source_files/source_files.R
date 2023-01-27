754afc26eb749f61cadc03878aec7c21e022c8d0
seresusly <carlos.resurreccion@obf.ateneo.edu>
2023-01-27 14:57:32 +0800

    renv

--- R4DS/source_files/source_files.R
+++ R4DS/source_files/source_files.R
@@ -2,10 +2,23 @@ pkgs <- c("tidyverse", "palmerpenguins", "nycflights13",
              "gapminder", "janitor", "styler", "babynames", 
              "usethis", "reprex", "ggridges", "hexbin", 
              "ggrepel", "arrow", "patchwork")
-for (x in pkgs){
-    #install.packages(x)
-    library(x, character.only = TRUE)
-}
+library(tidyverse)
+library(palmerpenguins)
+library(nycflights13)
+library(gapminder)
+library(janitor)
+library(styler)
+library(babynames)
+library(usethis)
+library(reprex)
+library(ggridges)
+library(hexbin)
+library(ggrepel)
+library(arrow)
+library(patchwork)
+# for (x in pkgs){
+#     library(x, character.only = TRUE)
+# }
 #deck <- read.csv("R4DS/deck.csv")
 rscripts <- Sys.glob("R4DS/*.R")
 sapply(rscripts, source)

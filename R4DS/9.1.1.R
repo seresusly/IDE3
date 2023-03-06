04c4723968098073cbeb5a21ea3cbefea21c5456
seresusly <carlos.resurreccion@obf.ateneo.edu>
2023-03-06 15:36:02 +0800

    push

--- R4DS/9.1.1.R
+++ R4DS/9.1.1.R
@@ -1,9 +1,9 @@
-library(dplyr)
-library(nycflights13)
-
-not_cancelled <- flights |> 
-  filter(!is.na(dep_delay), !is.na(arr_delay))
-
-not_cancelled |> 
-  group_by(year, month, day) |> 
-  summarize(mean = mean(dep_delay))
+# library(dplyr)
+# library(nycflights13)
+# 
+# not_cancelled <- flights |> 
+#   filter(!is.na(dep_delay), !is.na(arr_delay))
+# 
+# not_cancelled |> 
+#   group_by(year, month, day) |> 
+#   summarize(mean = mean(dep_delay))
